00b22ad217beb16b4ec8b75ce8dccba7b3d84fa5
lad02_003 <6820230003@tsu.ac.th>
2025-12-08 12:08:30 +0700

    Add imgage profile

--- lib/pages/myfirstpage.dart
+++ lib/pages/myfirstpage.dart
@@ -13,11 +13,22 @@ class MyFirstPage extends StatelessWidget {
         title: Text("My Profile"),
         backgroundColor: const Color.fromARGB(255, 64, 226, 255),
       ),
-      body: Column(
-        children: [
-          Text("ดุลยวัต", style: TextStyle(fontSize: 22),),
-          Text("วัตนะเสณร์",style: TextStyle(fontSize: 22),), 
-        ],
+      
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            CircleAvatar(
+              radius: 100,
+              backgroundImage: AssetImage("chin/small.jpg"),
+            ),
+            Text("ดุลยวัต", style: TextStyle(fontSize: 22,color:Color.fromARGB(255, 64, 255, 214)),),
+            Text("วัตนะเสณร์",style: TextStyle(fontSize: 22,color:const Color.fromARGB(255, 64, 255, 74)),), 
+            Text("6820230003", style: TextStyle(fontSize: 22,color:const Color.fromARGB(255, 115, 64, 255)),),
+            Text("6820230003@tsu.ac.th",style: TextStyle(fontSize: 22,color:const Color.fromARGB(255, 255, 64, 239)),),
+          ],
+        ),
       ),
     );
   }
